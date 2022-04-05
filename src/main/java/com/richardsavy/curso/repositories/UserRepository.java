d1f561ba4015d0408f832921ca5286c3248b5627
Richard Savy <richardsavy@gmail.com>
2022-04-05 07:22:18 -0300

    JPA repository

---
+++ src/main/java/com/richardsavy/curso/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.richardsavy.curso.repositories;
+
+import com.richardsavy.curso.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository <User, Long> {
+
+}
